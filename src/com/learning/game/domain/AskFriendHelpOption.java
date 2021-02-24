6403c37ee5273774789a3a47a3449c1eb92a1de7
dimashpro <sergiu.proca@gmail.com>
2021-02-24 21:54:13 +0100

    added help options

---
+++ src/com/learning/game/domain/AskFriendHelpOption.java
@@ -0,0 +1,11 @@
+package com.tekwill.learning.trivia.game.domain;
+
+public class AskFriendHelpOption extends HelpOption {
+
+    public void invoke(Question question) {
+        System.out.println("    (\uD83D\uDDE3) -> Friend tells that most probably is this one - " + question.getCorrectAnswer() + " - 99.7%");
+        setUsed(true);
+        setInvoked(false);
+    }
+
+}
---
+++ src/com/learning/game/domain/HelpOption.java
@@ -0,0 +1,29 @@
+package com.tekwill.learning.trivia.game.domain;
+
+abstract class HelpOption {
+
+    private boolean isUsed = false;
+    private boolean isInvoked = false;
+
+    abstract void invoke(Question question);
+
+    public boolean isUsed() {
+
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+
+        isUsed = used;
+    }
+
+    public boolean isInvoked() {
+
+        return isInvoked;
+    }
+
+    public void setInvoked(boolean invoked) {
+
+        isInvoked = invoked;
+    }
+}
