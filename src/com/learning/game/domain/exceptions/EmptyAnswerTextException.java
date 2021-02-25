388cc8b70256ef7cab983f71ed9f4a00d8bb132d
dimashpro <sergiu.proca@gmail.com>
2021-02-25 20:41:08 +0100

    added EmptyAnswerTextException

--- src/com/learning/game/domain/exceptions/EmptyAnswerTextException.java
+++ src/com/learning/game/domain/exceptions/EmptyAnswerTextException.java
@@ -1,4 +1,4 @@
-package com.tekwill.learning.trivia.game.domain.exceptions;
+package com.learning.game.domain.exceptions;
 
 public class EmptyAnswerTextException extends RuntimeException{
     public EmptyAnswerTextException(String s) {
